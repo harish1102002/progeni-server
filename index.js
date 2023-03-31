96b8c8998faf981a9175ce400d2f650017710626
HARISH R P <harishpranesh983@gmail.com>
2023-03-31 12:27:21 +0530

    1

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const m = require('mongodb').MongoClient;
 let post=[],col=null
 app.use(exp.json())
 app.use(cors({
-    origin:"",
-    credentials: true
+    origin:"*"
 }))
 
-m.connect("mongodb+srv://harish:harish7@cluster0.ffgjypz.mongodb.net/dummy").then((c)=>
-col=c.db().collection("dummy")).then(()=>{
+m.connect("mongodb+srv://harish:harish7@cluster0.ffgjypz.mongodb.net/progeni").then((c)=>
+col=c.db().collection("progeni")).then(()=>{
     col.find().forEach(e => post.push(e));});
-
+    
 app.get('/',(req,res)=>res.json(post))
+
+app.post('/',(req,res)=>{
+    col.insertOne(req.body.post)
+    post.push(req.body.post)
+    res.json(post)})
 
 app.listen(process.env.PORT||8000)
