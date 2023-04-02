256edde9889e3c90e0aa7f32c5ee56b09ab8cc95
HARISH R P <harishpranesh983@gmail.com>
2023-04-02 07:07:39 +0530

    admin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/',(req,res)=>{
     res.json(post)})
 
 app.post('/1',(res,req)=>{
-    col.updateOne({"id":req.body.id},{$set:{"ts":"Paid"}});
+    col.insertOne(req.body.post)
     for(let i=0;i<post.length;i++)
     if(post[i].id==req.body.id)
     post[i].ts="Paid";
