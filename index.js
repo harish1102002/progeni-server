e3b54cd5ec0f0ade30447a20ab796820fa82e519
HARISH R P <harishpranesh983@gmail.com>
2023-04-02 12:54:33 +0530

    final

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const m = require('mongodb').MongoClient;
 let post=[],col=null
 app.use(exp.json())
 app.use(cors({
-    origin:["https://progeni23.tech/register","https://progeni23.tech/login","https://progeni23.tech/phd/admin3"]
+    origin:["https://progeni23.tech/register","https://progeni23.tech/login","https://progeni23.tech/phd/admin3"],
+    credentials: true
 }));
 
 m.connect("mongodb+srv://harish:harish7@cluster0.ffgjypz.mongodb.net/progeni").then((c)=>
